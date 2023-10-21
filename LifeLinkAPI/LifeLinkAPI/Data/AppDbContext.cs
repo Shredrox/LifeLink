d3a0ba764557d1474266d7b1a317dc6e09592f6a
Shredrox <93082689+Shredrox@users.noreply.github.com>
2023-10-21 12:01:49 +0300

    Updated db context

--- LifeLinkAPI/LifeLinkAPI/Data/AppDbContext.cs
+++ LifeLinkAPI/LifeLinkAPI/Data/AppDbContext.cs
@@ -1,9 +1,28 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using LifeLinkAPI.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace LifeLinkAPI.Data
 {
     public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
 
+        }
+
+        public DbSet<Patient> Patients { get; set; }
+        public DbSet<Doctor> Doctors { get; set; }
+        //public DbSet<PrivateMedical> PrivateMedicals { get; set; }
+        public DbSet<Admin> Admins { get; set; }
+        public DbSet<CEO> Ceos { get; set; }
+        public DbSet<Hospital> Hospitals { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<MedicalRecord> MedicalRecords { get; set; }
+        public DbSet<LabTest> LabTests { get; set; }
+        public DbSet<Appointment> Appointments { get; set; }
+        public DbSet<Prescription> Prescriptions { get; set; }
+        public DbSet<Medication> Medications { get; set; }
+        public DbSet<Payment> Payments { get; set; }
+        public DbSet<Schedule> Schedules { get; set; }
     }
 }
