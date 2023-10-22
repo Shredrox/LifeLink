ddcc13b17f16e93780ba5dc59cc329a401df9061
Shredrox <93082689+Shredrox@users.noreply.github.com>
2023-10-22 17:54:07 +0300

    Added hospitalstay model

---
+++ LifeLinkAPI/LifeLinkAPI/Models/HospitalStay.cs
@@ -0,0 +1,15 @@
+﻿namespace LifeLinkAPI.Models
+{
+    public class HospitalStay
+    {
+        public int Id { get; set; }
+        public string AdmissionDate { get; set; }
+        public string DischargeDate { get; set; }
+        public string RoomNumber {  get; set; }
+        public string Cost { get; set; }
+        public int HospitalId { get; set; }
+        public Hospital Hospital { get; set; }
+        public int MedicalRecordId { get; set; }
+        public MedicalRecord MedicalRecord { get; set; }
+    }
+}
