54e6fdbc4b70aff84a50481608155734f96bb363
Hristo Zagorliev <hristo.zagorliev44@gmail.com>
2023-10-22 16:15:34 +0300

    Added log in and sign up pages

---
+++ LifeLinkUI/lifelink-ui/app/login/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <main>
+      Log In
+    </main>
+  )
+}
+
+export default page
---
+++ LifeLinkUI/lifelink-ui/app/signup/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <main>
+      Sign Up
+    </main>
+  )
+}
+
+export default page
