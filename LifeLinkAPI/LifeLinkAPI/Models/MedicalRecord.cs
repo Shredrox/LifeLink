257bb139e58e30be12c06c1ee421c587342fc1de
Shredrox <93082689+Shredrox@users.noreply.github.com>
2023-10-23 18:53:28 +0300

    Updated MedicalRecord

--- LifeLinkAPI/LifeLinkAPI/Models/MedicalRecord.cs
+++ LifeLinkAPI/LifeLinkAPI/Models/MedicalRecord.cs
@@ -6,12 +6,18 @@
         public required string Gender { get; set; }
         public required string Address { get; set; }
         public DateTime BirthDate { get; set; }
-        public List<Diagnosis> Diagnoses {  get; set; }
-        public List<Treatment> Treatments { get; set; }
+        public DateTime RegistrationDate { get; set; }
+        public List<Diagnosis>? Diagnoses {  get; set; }
+        public List<Treatment>? Treatments { get; set; }
         public List<LabTest>? LabTests { get; set; }
         public List<Prescription>? Prescriptions { get; set; }
         public List<HospitalStay>? HospitalStays { get; set; }
         public int PatientId { get; set; }
         public Patient Patient { get; set; }
+
+        public MedicalRecord()
+        {
+            RegistrationDate = DateTime.Now.ToUniversalTime();
+        }
     }
 }
