2c2ff6c3ece6891b7d6f3a71fbf5904e8de7d46f
Shredrox <93082689+Shredrox@users.noreply.github.com>
2023-10-31 14:59:17 +0200

    Added UserController with Login/Register/GetAllUsers

---
+++ LifeLinkAPI/LifeLinkAPI/Controllers/UserController.cs
@@ -0,0 +1,62 @@
+﻿using LifeLinkAPI.Models.DTOs;
+using LifeLinkAPI.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LifeLinkAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+        private readonly IAuthService _authService;
+
+        public UserController(
+            IUserService userService,
+            IAuthService authService)
+        {
+            _userService = userService;
+            _authService = authService;
+        }
+
+        [HttpPost("Register")]
+        public async Task<IActionResult> Register(UserDTO request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            await _userService.RegisterPatient(request);
+
+            return CreatedAtAction("Register", "Patient Registered");
+        }
+
+        [HttpPost("Login")]
+        public ActionResult Login(UserDTO request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            //check user
+            var user = _userService.GetUser(request);
+
+            if(user is null)
+            {
+                return BadRequest();
+            }
+
+            string token = _authService.CreateToken(user);
+
+            return Ok(token);
+        }
+
+        [HttpGet("GetAllUsers")]
+        public ActionResult GetAllUsers()
+        {
+            return Ok(_userService.GetAllUsers());
+        }
+    }
+}
