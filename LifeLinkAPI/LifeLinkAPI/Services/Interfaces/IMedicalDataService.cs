1b32cf129b31154f2db466b3e6f395e9478df6c9
Shredrox <93082689+Shredrox@users.noreply.github.com>
2023-10-31 15:03:05 +0200

    Added MedicalDataService

---
+++ LifeLinkAPI/LifeLinkAPI/Services/Interfaces/IMedicalDataService.cs
@@ -0,0 +1,9 @@
+﻿using LifeLinkAPI.Models.DTOs;
+
+namespace LifeLinkAPI.Services.Interfaces
+{
+    public interface IMedicalDataService
+    {
+        public Task CreateAppointment(AppointmentDTO request);
+    }
+}
