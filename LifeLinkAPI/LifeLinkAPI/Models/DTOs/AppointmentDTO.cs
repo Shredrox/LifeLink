af4154809e0ee0545cb601cbbe7edea5ee48da1c
Shredrox <93082689+Shredrox@users.noreply.github.com>
2023-10-31 15:02:43 +0200

    Added AppointmentDTO

---
+++ LifeLinkAPI/LifeLinkAPI/Models/DTOs/AppointmentDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace LifeLinkAPI.Models.DTOs
+{
+    public class AppointmentDTO
+    {
+        public string PatientName { get; set; }
+        public string MedicalName { get; set; }
+        public DateTime Date {  get; set; } 
+    }
+}
