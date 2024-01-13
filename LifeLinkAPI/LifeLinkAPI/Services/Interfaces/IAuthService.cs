213977178f80c4f736aaddd64b9c1195cf2a312d
Shredrox <93082689+Shredrox@users.noreply.github.com>
2024-01-13 13:16:42 +0200

    Update AuthService

--- LifeLinkAPI/LifeLinkAPI/Services/Interfaces/IAuthService.cs
+++ LifeLinkAPI/LifeLinkAPI/Services/Interfaces/IAuthService.cs
@@ -5,5 +5,7 @@ namespace LifeLinkAPI.Services.Interfaces
     public interface IAuthService
     {
         public string CreateToken(User user);
+        public Task<string> CreateRefreshToken(User user);
+        public Task<User?> GetUserFromRefreshToken(string refreshToken);
     }
 }
