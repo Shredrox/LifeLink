cb849bbd9fdf006d4121913eebb961573d639a26
Shredrox <93082689+Shredrox@users.noreply.github.com>
2024-01-13 13:27:31 +0200

    Update IUserService.cs

--- LifeLinkAPI/LifeLinkAPI/Services/Interfaces/IUserService.cs
+++ LifeLinkAPI/LifeLinkAPI/Services/Interfaces/IUserService.cs
@@ -7,7 +7,11 @@ namespace LifeLinkAPI.Services.Interfaces
     {
         public Task RegisterPatient(UserDTO request);
         public User? GetUser(UserDTO request);
+        public Task<User?> GetUserById(int id);
+        public Task Update(User user);
+        public User? GetUserByName(string name);
         public List<User> GetAllUsers();
+        public Task RegisterAdmin();
         public Task CreateDoctor(User request);
         public Task CreateAdmin(User request);
         public Task CreateHospitalAdmin(User request);
