c1d5a7fa68f36cda24b6eb10395b8ff581a4e628
Shredrox <93082689+Shredrox@users.noreply.github.com>
2024-01-13 13:16:23 +0200

    Update User.cs

--- LifeLinkAPI/LifeLinkAPI/Models/User.cs
+++ LifeLinkAPI/LifeLinkAPI/Models/User.cs
@@ -6,6 +6,8 @@
         public string Username { get; set; }
         public string Password { get; set; }
         public string Email { get; set; }
+        public string? RefreshToken { get; set; }
+        public DateTime? RefreshTokenValidity { get; set; }
         public Role Role { get; set; }
     }
 
