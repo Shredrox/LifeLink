00ae19e024acaf2c5ece262bc14d1897df1cd180
Shredrox <93082689+Shredrox@users.noreply.github.com>
2024-01-13 13:28:05 +0200

    Update UserService.cs

--- LifeLinkAPI/LifeLinkAPI/Services/UserService.cs
+++ LifeLinkAPI/LifeLinkAPI/Services/UserService.cs
@@ -2,6 +2,7 @@
 using LifeLinkAPI.Models;
 using LifeLinkAPI.Models.DTOs;
 using LifeLinkAPI.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace LifeLinkAPI.Services
 {
@@ -45,6 +46,58 @@ namespace LifeLinkAPI.Services
             await _context.SaveChangesAsync();
         }
 
+        public async Task RegisterDoctor(UserDTO request)
+        {
+            string passwordHash = BCrypt.Net.BCrypt.EnhancedHashPassword(request.Password);
+
+            var user = new User
+            {
+                Username = request.Username,
+                Password = passwordHash,
+                Email = request.Email,
+                Role = Role.Patient
+            };
+
+            var patient = new Patient
+            {
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                SSN = "1",
+                PhoneNumber = request.PhoneNumber,
+                MedicalRecord = new MedicalRecord
+                {
+                    Gender = request.Gender,
+                    Address = request.Address,
+                    BirthDate = new DateTime(2023, 10, 1).ToUniversalTime()
+                },
+                User = user
+            };
+
+            _context.Patients.Add(patient);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task RegisterAdmin()
+        {
+            string passwordHash = BCrypt.Net.BCrypt.EnhancedHashPassword("admin123");
+
+            var user = new User
+            {
+                Username = "Admin",
+                Password = passwordHash,
+                Email = "admin@emai",
+                Role = Role.Admin
+            };
+
+            var admin = new Admin
+            {
+                User = user
+            };
+
+            _context.Admins.Add(admin);
+            await _context.SaveChangesAsync();
+        }
+
         public User? GetUser(UserDTO request)
         {
             var user = _context.Users.FirstOrDefault(user => user.Username == request.Username || user.Email == request.Email);
@@ -62,6 +115,31 @@ namespace LifeLinkAPI.Services
             }
 
             return user;
+        }
+
+        public async Task<User?> GetUserById(int id)
+        {
+            return await _context.Users.FirstOrDefaultAsync(u => u.Id == id);
+        }
+
+        public async Task Update(User user)
+        {
+            var existingUser = await _context.Users.FirstOrDefaultAsync(u => u.Id == user.Id);
+
+            if(existingUser == null)
+            {
+                return;
+            }
+
+            existingUser.RefreshToken = user.RefreshToken;
+            existingUser.RefreshTokenValidity = user.RefreshTokenValidity;
+
+            await _context.SaveChangesAsync();
+        }
+
+        public User? GetUserByName(string name)
+        {
+            return _context.Users.FirstOrDefault(u => u.Username == name);
         }
 
         public List<User> GetAllUsers()
