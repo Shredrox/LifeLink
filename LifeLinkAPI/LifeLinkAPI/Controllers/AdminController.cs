6f0781b9a1427d00a9b9bbbf576ab8b872127e66
Shredrox <93082689+Shredrox@users.noreply.github.com>
2024-01-13 13:15:48 +0200

    Create base AdminController

---
+++ LifeLinkAPI/LifeLinkAPI/Controllers/AdminController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LifeLinkAPI.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+        public AdminController()
+        {
+
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpPost("RegisterHospital")]
+        public ActionResult RegisterHospital([FromBody] string value)
+        {
+            return Ok(value);
+
+        }
+    }
+}
