f9e7caf7e268766de9716c26b6d277ad05ce0c38
Shredrox <93082689+Shredrox@users.noreply.github.com>
2024-03-11 23:13:50 +0200

    Delete AppointmentDTO

--- LifeLinkAPI/LifeLinkAPI.Application/DTOs/AppointmentDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace LifeLinkAPI.Application.DTOs
-{
-    public class AppointmentDTO
-    {
-        public string PatientName { get; set; }
-        public string MedicalName { get; set; }
-        public DateTime Date {  get; set; } 
-    }
-}
