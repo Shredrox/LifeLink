d96830b77a215d46de2b42a5f0310a232b4f0b42
Shredrox <93082689+Shredrox@users.noreply.github.com>
2024-03-11 22:21:02 +0200

    Create RegisterRequestDto

---
+++ LifeLinkAPI/LifeLinkAPI.Application/DTOs/Requests/RegisterRequestDto.cs
@@ -0,0 +1,11 @@
+﻿namespace LifeLinkAPI.Application.DTOs.Requests;
+
+public record RegisterRequestDto(
+    string FirstName,
+    string LastName,
+    string Username,
+    string Email,
+    string Password,
+    string PhoneNumber,
+    string Gender,
+    string Address);
