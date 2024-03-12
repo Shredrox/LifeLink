fae4761354496945c84a70d80f2a404aadd988b4
Shredrox <93082689+Shredrox@users.noreply.github.com>
2024-03-12 12:42:17 +0200

    Create AppointmentHour model

---
+++ LifeLinkAPI/LifeLinkAPI.Domain/Models/AppointmentHour.cs
@@ -0,0 +1,10 @@
+﻿namespace LifeLinkAPI.Domain.Models;
+
+public class AppointmentHour
+{
+    public int Id { get; set; }
+    public DateTime Time { get; set; }
+    public bool Booked { get; set; }
+    public int ScheduleId { get; set; }
+    public Schedule Schedule { get; set; }
+}
