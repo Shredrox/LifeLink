88a4a0e69adb56d766437bec54295d393e3e4769
Shredrox <93082689+Shredrox@users.noreply.github.com>
2024-03-12 09:10:37 +0200

    Create DependencyInjection for adding application services

---
+++ LifeLinkAPI/LifeLinkAPI.Application/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using LifeLinkAPI.Application.Interfaces.IServices;
+using LifeLinkAPI.Application.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LifeLinkAPI.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddScoped<IUserService, UserService>();
+        services.AddScoped<IAuthService, AuthService>();
+        services.AddScoped<ITokenService, TokenService>();
+        services.AddScoped<IMedicalDataService, MedicalDataService>();
+        services.AddScoped<IAppointmentService, AppointmentService>();
+        
+        return services;
+    }
+}
