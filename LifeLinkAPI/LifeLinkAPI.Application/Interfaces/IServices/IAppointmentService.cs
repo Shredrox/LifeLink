c6de9792f8af19091888000eb6b9987ba476647f
Shredrox <93082689+Shredrox@users.noreply.github.com>
2024-03-12 21:09:44 +0200

    Update AppointmentService

--- LifeLinkAPI/LifeLinkAPI.Application/Interfaces/IServices/IAppointmentService.cs
+++ LifeLinkAPI/LifeLinkAPI.Application/Interfaces/IServices/IAppointmentService.cs
@@ -1,6 +1,10 @@
-﻿namespace LifeLinkAPI.Application.Interfaces.IServices;
+﻿using LifeLinkAPI.Application.DTOs.Requests;
+using LifeLinkAPI.Application.DTOs.Responses;
+
+namespace LifeLinkAPI.Application.Interfaces.IServices;
 
 public interface IAppointmentService
 {
-    
+    Task CreateAppointment(BookAppointmentRequestDto request);
+    Task<AppointmentHoursResponseDto> GetAllAppointmentHoursByDoctorAndDate(int doctorId, DateTime date);
 }
