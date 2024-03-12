14ffe441033e599eec9f1209bfd2fbcede37e56a
Shredrox <93082689+Shredrox@users.noreply.github.com>
2024-03-12 19:27:15 +0200

    Create DoctorRepository

---
+++ LifeLinkAPI/LifeLinkAPI.Application/Interfaces/IRepositories/IDoctorRepository.cs
@@ -0,0 +1,8 @@
+﻿using LifeLinkAPI.Domain.Models;
+
+namespace LifeLinkAPI.Application.Interfaces.IRepositories;
+
+public interface IDoctorRepository
+{
+    Task<Doctor?> GetDoctorById(int id);
+}
---
+++ LifeLinkAPI/LifeLinkAPI.Infrastructure/Repositories/DoctorRepository.cs
@@ -0,0 +1,22 @@
+﻿using LifeLinkAPI.Application.Interfaces.IRepositories;
+using LifeLinkAPI.Domain.Models;
+using LifeLinkAPI.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace LifeLinkAPI.Infrastructure.Repositories;
+
+public class DoctorRepository : IDoctorRepository
+{
+    private readonly LifeLinkDbContext _context;
+
+    public DoctorRepository(LifeLinkDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<Doctor?> GetDoctorById(int id)
+    {
+        return await _context.Doctors
+            .FirstOrDefaultAsync(d => d.Id == id);
+    }
+}
