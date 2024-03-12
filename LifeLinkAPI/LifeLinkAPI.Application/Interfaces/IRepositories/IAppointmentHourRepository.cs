ca0abc517702c152bd92def5e5f01f08bac196ba
Shredrox <93082689+Shredrox@users.noreply.github.com>
2024-03-12 19:27:00 +0200

    Create AppointmentHourRepository

---
+++ LifeLinkAPI/LifeLinkAPI.Application/Interfaces/IRepositories/IAppointmentHourRepository.cs
@@ -0,0 +1,9 @@
+﻿using LifeLinkAPI.Domain.Models;
+
+namespace LifeLinkAPI.Application.Interfaces.IRepositories;
+
+public interface IAppointmentHourRepository
+{
+    Task<AppointmentHour?> GetAppointmentHourById(int id);
+    Task<List<AppointmentHour>> GetAllAppointmentHoursByDoctor(int doctorId);
+}
