c851a2b65c99ef95ba74e801f66fabd720be0197
Shredrox <93082689+Shredrox@users.noreply.github.com>
2024-03-12 21:08:09 +0200

    Add RegisterDoctor to DoctorService

--- LifeLinkAPI/LifeLinkAPI.Application/Interfaces/IServices/IDoctorService.cs
+++ LifeLinkAPI/LifeLinkAPI.Application/Interfaces/IServices/IDoctorService.cs
@@ -1,6 +1,8 @@
-﻿namespace LifeLinkAPI.Application.Interfaces.IServices;
+﻿using LifeLinkAPI.Application.DTOs.Requests;
+
+namespace LifeLinkAPI.Application.Interfaces.IServices;
 
 public interface IDoctorService
 {
-    
+    Task RegisterDoctor(RegisterDoctorRequestDto request);
 }
--- LifeLinkAPI/LifeLinkAPI.Application/Services/DoctorService.cs
+++ LifeLinkAPI/LifeLinkAPI.Application/Services/DoctorService.cs
@@ -1,8 +1,49 @@
-﻿using LifeLinkAPI.Application.Interfaces.IServices;
+﻿using LifeLinkAPI.Application.DTOs.Requests;
+using LifeLinkAPI.Application.Interfaces.IRepositories;
+using LifeLinkAPI.Application.Interfaces.IServices;
+using LifeLinkAPI.Domain.Models;
 
 namespace LifeLinkAPI.Application.Services;
 
 public class DoctorService : IDoctorService
 {
-    
+    private readonly IDoctorRepository _doctorRepository;
+    private readonly IUserRepository _userRepository;
+
+    public DoctorService(
+        IDoctorRepository doctorRepository, 
+        IUserRepository userRepository)
+    {
+        _doctorRepository = doctorRepository;
+        _userRepository = userRepository;
+    }
+
+    public async Task RegisterDoctor(RegisterDoctorRequestDto request)
+    {
+        var user = new User
+        {
+            UserName = request.Username,
+            Email = request.Email,
+            Role = Role.Doctor
+        };
+
+        await _userRepository.Add(user, request.Password);
+
+        var doctor = new Doctor
+        {
+            FirstName = request.FirstName,
+            LastName = request.LastName,
+            PhoneNumber = request.PhoneNumber,
+            Specialty = request.Specialty,
+            Experience = request.Experience,
+            Age = request.Age,
+            Schedule = new Schedule
+            {
+                WorkHours = request.WorkHours
+            },
+            User = user
+        };
+        
+        await _doctorRepository.Add(doctor);
+    }
 }
