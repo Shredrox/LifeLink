03077fdcc808aa574b34c7ba6b7463fbd34ec44e
Shredrox <93082689+Shredrox@users.noreply.github.com>
2024-03-12 21:09:50 +0200

    Create DoctorController

---
+++ LifeLinkAPI/LifeLinkAPI.Web/Controllers/DoctorController.cs
@@ -0,0 +1,27 @@
+using LifeLinkAPI.Application.DTOs.Requests;
+using LifeLinkAPI.Application.Interfaces.IServices;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LifeLinkAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DoctorController : ControllerBase
+    {
+        private readonly IDoctorService _doctorService;
+
+        public DoctorController(IDoctorService doctorService)
+        {
+            _doctorService = doctorService;
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpPost]
+        public async Task<IActionResult> RegisterDoctor([FromBody] RegisterDoctorRequestDto request)
+        {
+            await _doctorService.RegisterDoctor(request);
+            return Ok();
+        }
+    }
+}
