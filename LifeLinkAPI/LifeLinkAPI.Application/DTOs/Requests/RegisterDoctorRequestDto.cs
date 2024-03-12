9fafdcc4aa5c4733d2f2a60d7d09f6a6417c0418
Shredrox <93082689+Shredrox@users.noreply.github.com>
2024-03-12 21:07:23 +0200

    Create RegisterDoctorRequestDto

---
+++ LifeLinkAPI/LifeLinkAPI.Application/DTOs/Requests/RegisterDoctorRequestDto.cs
@@ -0,0 +1,13 @@
+﻿namespace LifeLinkAPI.Application.DTOs.Requests;
+
+public record RegisterDoctorRequestDto(
+    string Username,
+    string Email,
+    string Password,
+    string FirstName,
+    string LastName,
+    string Specialty,
+    string Experience,
+    int Age,
+    string PhoneNumber,
+    string WorkHours);
