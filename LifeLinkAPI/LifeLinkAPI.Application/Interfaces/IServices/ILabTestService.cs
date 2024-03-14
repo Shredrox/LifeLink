514d1c1abea1387a20c1183b8dc783ed99a270c2
Shredrox <93082689+Shredrox@users.noreply.github.com>
2024-03-14 18:05:27 +0200

    Create LabTestService

---
+++ LifeLinkAPI/LifeLinkAPI.Application/Interfaces/IServices/ILabTestService.cs
@@ -0,0 +1,8 @@
+﻿using LifeLinkAPI.Application.DTOs.Requests;
+
+namespace LifeLinkAPI.Application.Interfaces.IServices;
+
+public interface ILabTestService
+{
+    Task AddLabTest(AddLabTestRequestDto request, int medicalRecordId);
+}
---
+++ LifeLinkAPI/LifeLinkAPI.Application/Services/LabTestService.cs
@@ -0,0 +1,40 @@
+﻿using LifeLinkAPI.Application.DTOs.Requests;
+using LifeLinkAPI.Application.Interfaces.IRepositories;
+using LifeLinkAPI.Application.Interfaces.IServices;
+using LifeLinkAPI.Domain.Exceptions;
+using LifeLinkAPI.Domain.Models;
+
+namespace LifeLinkAPI.Application.Services;
+
+public class LabTestService : ILabTestService
+{
+    private readonly ILabTestRepository _labTestRepository;
+    private readonly IMedicalRecordRepository _medicalRecordRepository;
+
+    public LabTestService(
+        ILabTestRepository labTestRepository, 
+        IMedicalRecordRepository medicalRecordRepository)
+    {
+        _labTestRepository = labTestRepository;
+        _medicalRecordRepository = medicalRecordRepository;
+    }
+
+    public async Task AddLabTest(AddLabTestRequestDto request, int medicalRecordId)
+    {
+        var medicalRecord = await _medicalRecordRepository.GetMedicalRecordById(medicalRecordId);
+        if (medicalRecord is null)
+        {
+            throw new MedicalRecordNotFoundException();
+        }
+
+        var labTest = new LabTest
+        {
+            Name = request.Name,
+            Result = request.Result,
+            Cost = request.Cost,
+            MedicalRecord = medicalRecord
+        };
+
+        await _labTestRepository.InsertLabTest(labTest);
+    }
+}
