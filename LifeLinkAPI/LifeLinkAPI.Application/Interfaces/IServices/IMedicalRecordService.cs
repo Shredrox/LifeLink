af50a7de7cc2fea2552b68075fc4f4635b75789a
Shredrox <93082689+Shredrox@users.noreply.github.com>
2024-03-14 10:59:13 +0200

    Add AddDiagnosisToMedicalRecord to MedicalRecordService

--- LifeLinkAPI/LifeLinkAPI.Application/Interfaces/IServices/IMedicalRecordService.cs
+++ LifeLinkAPI/LifeLinkAPI.Application/Interfaces/IServices/IMedicalRecordService.cs
@@ -1,9 +1,9 @@
-﻿using LifeLinkAPI.Application.DTOs;
+﻿using LifeLinkAPI.Application.DTOs.Requests;
 
 namespace LifeLinkAPI.Application.Interfaces.IServices
 {
     public interface IMedicalRecordService
     {
-        
+        Task AddDiagnosisToMedicalRecord(AddDiagnosisRequestDto request, int medicalRecordId);
     }
 }
--- LifeLinkAPI/LifeLinkAPI.Application/Services/MedicalRecordService.cs
+++ LifeLinkAPI/LifeLinkAPI.Application/Services/MedicalRecordService.cs
@@ -1,10 +1,40 @@
-﻿using LifeLinkAPI.Application.DTOs;
+﻿using LifeLinkAPI.Application.DTOs.Requests;
+using LifeLinkAPI.Application.Interfaces.IRepositories;
 using LifeLinkAPI.Application.Interfaces.IServices;
+using LifeLinkAPI.Domain.Exceptions;
+using LifeLinkAPI.Domain.Models;
 
 namespace LifeLinkAPI.Application.Services
 {
     public class MedicalRecordService : IMedicalRecordService
     {
-        
+        private readonly IMedicalRecordRepository _medicalRecordRepository;
+
+        public MedicalRecordService(IMedicalRecordRepository medicalRecordRepository)
+        {
+            _medicalRecordRepository = medicalRecordRepository;
+        }
+
+        public async Task AddDiagnosisToMedicalRecord(AddDiagnosisRequestDto request, int medicalRecordId)
+        {
+            var medicalRecord = await _medicalRecordRepository.GetMedicalRecordById(medicalRecordId);
+            if (medicalRecord is null)
+            {
+                throw new MedicalRecordNotFoundException();
+            }
+            
+            var diagnosis = new Diagnosis
+            {
+                Name = request.Name,
+                Description = request.Description,
+                Illness = new Illness
+                {
+                    Name = request.IllnessName
+                }
+            };
+
+            medicalRecord.Diagnoses.Add(diagnosis);
+            await _medicalRecordRepository.UpdateMedicalRecord(medicalRecord);
+        }
     }
 }
