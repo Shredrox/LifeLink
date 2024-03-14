c9cd31051f62ca205496a9c321cfbe42bb23b1b8
Shredrox <93082689+Shredrox@users.noreply.github.com>
2024-03-14 12:01:39 +0200

    Update Diagnosis model

--- LifeLinkAPI/LifeLinkAPI.Domain/Models/Diagnosis.cs
+++ LifeLinkAPI/LifeLinkAPI.Domain/Models/Diagnosis.cs
@@ -5,6 +5,10 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public int MedicalRecordId { get; set; }
+        public MedicalRecord MedicalRecord { get; set; }
+        public int DoctorId { get; set; }
+        public Doctor Doctor { get; set; }
         public int IllnessId { get; set; }
         public Illness Illness { get; set; }
     }
