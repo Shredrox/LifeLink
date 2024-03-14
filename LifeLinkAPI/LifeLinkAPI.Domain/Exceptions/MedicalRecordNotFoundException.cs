69f6fa546a151cd5c844b00af385146b4b31392d
Shredrox <93082689+Shredrox@users.noreply.github.com>
2024-03-14 10:58:34 +0200

    Create MedicalRecordNotFoundException

---
+++ LifeLinkAPI/LifeLinkAPI.Domain/Exceptions/MedicalRecordNotFoundException.cs
@@ -0,0 +1,18 @@
+﻿namespace LifeLinkAPI.Domain.Exceptions;
+
+public class MedicalRecordNotFoundException : Exception
+{
+    public MedicalRecordNotFoundException()
+    {
+    }
+
+    public MedicalRecordNotFoundException(string message) 
+        : base(message)
+    {
+    }
+    
+    public MedicalRecordNotFoundException(string message, Exception inner) 
+        : base(message, inner)
+    {
+    }
+}
