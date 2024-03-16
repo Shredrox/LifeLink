37a640d1830dfd01d86184a340bd1f54aedf2a0f
Shredrox <93082689+Shredrox@users.noreply.github.com>
2024-03-16 14:40:04 +0200

    Add Paid property to Payment model

--- LifeLinkAPI/LifeLinkAPI.Domain/Models/Payment.cs
+++ LifeLinkAPI/LifeLinkAPI.Domain/Models/Payment.cs
@@ -9,5 +9,6 @@
         public Doctor? Doctor { get; set; }
         public decimal Amount { get; set; }
         public string PaymentMethod { get; set; }
+        public bool Paid { get; set; }
     }
 }
