ea0a2d331287da89ec75f498024458621175c547
Shredrox <93082689+Shredrox@users.noreply.github.com>
2024-03-16 15:00:04 +0200

    Update Payment model

--- LifeLinkAPI/LifeLinkAPI.Domain/Models/Payment.cs
+++ LifeLinkAPI/LifeLinkAPI.Domain/Models/Payment.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
         public int PatientId { get; set; }
-        public Patient Patient { get; set; }
+        public Patient? Patient { get; set; }
         public int DoctorId { get; set; }
         public Doctor? Doctor { get; set; }
         public decimal Amount { get; set; }
