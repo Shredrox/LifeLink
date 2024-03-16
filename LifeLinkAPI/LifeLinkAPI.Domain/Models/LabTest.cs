5913882793c27e240abdb41fd292f6a92d387303
Shredrox <93082689+Shredrox@users.noreply.github.com>
2024-03-16 14:22:11 +0200

    Update LabTest model

--- LifeLinkAPI/LifeLinkAPI.Domain/Models/LabTest.cs
+++ LifeLinkAPI/LifeLinkAPI.Domain/Models/LabTest.cs
@@ -1,10 +1,13 @@
-﻿namespace LifeLinkAPI.Domain.Models
+﻿using LifeLinkAPI.Domain.Enums;
+
+namespace LifeLinkAPI.Domain.Models
 {
     public class LabTest
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Result { get; set; }
+        public string? Result { get; set; }
+        public LabTestResultStatus ResultStatus { get; set; }
         public string Cost { get; set; }
         public int MedicalRecordId { get; set; }
         public MedicalRecord MedicalRecord { get; set; }
