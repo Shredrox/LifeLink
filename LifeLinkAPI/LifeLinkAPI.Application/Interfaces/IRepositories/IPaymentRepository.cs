cc94c43a9e20c5114feecd7cc09c022ba048770c
Shredrox <93082689+Shredrox@users.noreply.github.com>
2024-03-16 14:59:31 +0200

    Create PaymentRepository

---
+++ LifeLinkAPI/LifeLinkAPI.Application/Interfaces/IRepositories/IPaymentRepository.cs
@@ -0,0 +1,10 @@
+﻿using LifeLinkAPI.Domain.Models;
+
+namespace LifeLinkAPI.Application.Interfaces.IRepositories;
+
+public interface IPaymentRepository
+{
+    Task<IEnumerable<Payment>> GetPaymentsByPatientId(int patientId);
+    Task<IEnumerable<Payment>> GetPaymentsByDoctorId(int doctorId);
+    Task InsertPayment(Payment payment);
+}
