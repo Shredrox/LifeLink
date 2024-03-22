09c7f9252e7fb1a8e03d2c0808434d0dfae5ae71
Shredrox <93082689+Shredrox@users.noreply.github.com>
2024-03-22 14:58:02 +0200

    Create AppointmentNotFountException

---
+++ LifeLinkAPI/LifeLinkAPI.Domain/Exceptions/AppointmentNotFountException.cs
@@ -0,0 +1,18 @@
+﻿namespace LifeLinkAPI.Domain.Exceptions;
+
+public class AppointmentNotFountException : Exception
+{
+    public AppointmentNotFountException()
+    {
+    }
+
+    public AppointmentNotFountException(string message) 
+        : base(message)
+    {
+    }
+    
+    public AppointmentNotFountException(string message, Exception inner) 
+        : base(message, inner)
+    {
+    }
+}
