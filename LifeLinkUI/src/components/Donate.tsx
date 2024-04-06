7737e1afdeee716a3150843494c51aed78abee5e
creed28 <hristo.zagorliev44@gmail.com>
2024-04-06 15:20:27 +0300

    Edit donate component

--- LifeLinkUI/src/components/Donate.tsx
+++ LifeLinkUI/src/components/Donate.tsx
@@ -1,24 +1,24 @@
-import Button from './Button'
-import DonateLogo from '../assets/images/donate.png'
+import Button from './Button';
+import DonateLogo from '../assets/images/donate.png';
 
 const Donate = () => {
   return (
-    <section className='bg-primaryBlue text-white h-[270px] flex items-center justify-center'>
-      <section className='flex justify-center gap-[6rem] mb-3'>
-        <div className='flex items-center'>
+    <section className='bg-[#173153] text-white h-[270px] flex items-center justify-center'>
+      <div className='flex gap-[6rem] mb-3'>
+        <div className='flex items-center gap-x-3'>
           <img src={DonateLogo} alt='Donate Logo' width={200} height={95} />
-          <p className='text-[2.1rem] font-bold w-[330px] text-center'>Your gift holds great power – donate today!</p>
+          <p className='text-[2.1rem] font-bold w-[290px] text-center'>Your gift holds great power – donate today!</p>
         </div>
-        <div className='flex items-center w-[560px] mb-4'>
-          <p className='w-[440px] text-[1.2rem]'>
+        <div className='flex items-center gap-x-3 w-[560px]'>
+          <p className='text-[1.2rem]'>
             Contribute a tax-deductible donation and join the forefront of transformative healthcare, 
             where connections and compassion are reshaping the field of medicine.
           </p>
-          <Button style='donateBtn mb-14' content='Donate' />
+          <Button style='btnEffects donateBtn mb-14' content='Donate' />
         </div>
-      </section>
+      </div>
     </section>
-  )
+  );
 }
 
-export default Donate
+export default Donate;
